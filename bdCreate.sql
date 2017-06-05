4feff97cce64426e7da0df54f896d806431a866d
Alexey Kapustin <djkah11@yandex.ru>
2017-06-05 21:33:19 +0300

    bd scheme

---
+++ bdCreate.sql
@@ -0,0 +1,16 @@
+CREATE TABLE generalInfo(
+usedID             varchar(80),
+appVersion varchar(40),
+compilerVersion real,
+typeCompiler varchar(10),
+language varchar(20),
+glslVersion  real,
+renderer varchar(50),
+vendor varchar(30),
+os    varchar(10),
+version varchar(15),
+qtVersion real,
+screenDpi int,
+screenHeight int,
+screenWidth int
+);
